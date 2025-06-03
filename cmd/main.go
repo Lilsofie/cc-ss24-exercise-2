70e24cbef76984313a81b9918257185b8067ec1e
Lilsofie <123223306+Lilsofie@users.noreply.github.com>
2025-06-03 20:46:03 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -430,10 +430,10 @@ func main() {
 	// one by yourself!
 	coll, err := prepareDatabase(client, "exercise-2", "information")
 
-	drop := coll.Drop(context.TODO())
-	if drop != nil {
-		log.Fatal("Failed to drop collection:", drop)
-	}
+	// drop := coll.Drop(context.TODO())
+	// if drop != nil {
+	// 	log.Fatal("Failed to drop collection:", drop)
+	// }
 
 	prepareData(client, coll)
 
