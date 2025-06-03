1d90b4a6ad1bf187eb00f3e711be96b59d511ca7
Lilsofie <123223306+Lilsofie@users.noreply.github.com>
2025-06-03 06:15:23 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -247,7 +247,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	uri := os.Getenv("MONGODB_URI")
+	uri := os.Getenv("DATABASE_URI")
 	if len(uri) == 0 {
 		fmt.Printf("failure to load env variable\n")
 		os.Exit(1)
